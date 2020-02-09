54fb8db766e07b364d0220216a5736eb6504c92d
ZeroShiro <q1311588796@gmail.com>
2020-02-09 14:12:01 +0800

    two-app

--- src/components/Orders/Pay.vue
+++ src/components/Orders/Pay.vue
@@ -94,7 +94,7 @@ export default {
     },
     pay() {
       const data = {
-        body: "米修在线",
+        body: "test",
         out_trade_no: new Date().getTime().toString(),
         total_fee: 1
       };
@@ -121,7 +121,6 @@ export default {
         if (res.err_msg == "get_brand_wcpay_request:ok") {
           // 使用以上方式判断前端返回,微信团队郑重提示：
           //res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
-          // alert("支付成功");
           // 生成订单
           this.addOrder();
         }
